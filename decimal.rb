8ae01495e4e3758b794b4351104dbdd3f39ab09c
Ján Staník <morb@indra.local>
2014-11-27 06:54:58 +0000

    Housekeeping.

--- decimal.rb
+++ decimal.rb
@@ -1,16 +1,13 @@
 
-#require 'Math'
 require 'rasem'
 
 def render ( width_mm, height_mm, count )
   # first tick
+  puts 0.0
   for i in 0..count
     # next tick
     upper = 10 ** i
     step = upper / 10
-    log = Math.log10( upper )
-    #x  = log * width_mm / count
-    #puts x
     if i > 0
       for j in 1..9
         x = Math.log10( 10 ** ( i - 1 ) + j * step ) * width_mm / count
@@ -20,5 +17,5 @@ def render ( width_mm, height_mm, count )
   end
 end
 
-render( 160, 15, 4 )
+render( 150, 15, 3 )
 
