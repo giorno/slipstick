139e7736bb84bba3c022a1aa7d084a184905c365
Jan Stanik <js@creat.io>
2014-12-03 18:31:01 +0000

    Refactor fodder generator.

--- src/decimal.rb
+++ src/decimal.rb
@@ -11,8 +11,10 @@ module Io::Creat::Slipstick
     def initialize ( parent, label, size, rel_off_x_mm, rel_off_y_mm, h_mm, flipped = false )
       super( parent, label, rel_off_x_mm, rel_off_y_mm, h_mm, flipped )
 
-      @size          = size
-      @constants     = {}
+      @size      = size
+      @constants = {}
+      @scale     = @w_mainscale_mm / @size
+      @start_mm  = @w_label_mm + @w_subscale_mm
     end
 
     # these constants will be added as explicit ticks with cursive names when render() is called
@@ -23,13 +25,8 @@ module Io::Creat::Slipstick
     end
 
     public
-    def add_subscale ( left_border_mm ) # disabled
-      @left_border_mm = left_border_mm
-    end
-
-    public
     def render ( )
-      last = @w_label_mm + @w_subscale_mm
+      last = @start_mm
       for i in 1..@size
         # next tick
         upper = 10 ** i
@@ -38,51 +35,54 @@ module Io::Creat::Slipstick
         for j in 0..18
           value = base + j * step
           # physical dimension coordinates
-          x = @w_label_mm + @w_subscale_mm + Math.log10( value ) * @w_mainscale_mm / @size
+          x = @start_mm + Math.log10( value ) * @scale
           h = @h_mm * ( j == 0 ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][0] : ( j % 2 == 0 ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][1] : @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][2] ) )
           if j < 18 # last one is not rendered, but is required for small ticks calculation
            render_tick( x, h, ( j % 2 ) == 0 ? "%d" % value : nil )
           end
 
           if j > 0
-            # fill the range with smallest ticks
-            delta = x - last
-            no_smallest = 0
-            @dim[Io::Creat::Slipstick::Key::FODDERS].each do | no |
-              if delta > no * @dim[Io::Creat::Slipstick::Key::CLEARING]
-                no_smallest = no
-                break
-              end
-            end
-
-            if no_smallest > 0
-              stepper = step / no_smallest
-              for k in 1..no_smallest - 1
-                mx = @w_label_mm + @w_subscale_mm + Math.log10( base + ( j  - 1 ) * step + k * stepper ) * @w_mainscale_mm / @size
-                h = @h_mm * ( k % ( no_smallest / 5 )  == 0 ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][3] : @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][4] )
-                render_tick( mx, h, nil )
-              end
-            end
+	    last = render_fodder( last, x, base + ( j - 1 ) * step, step )
           end
           last = x
         end
       end
       # last tick
-      render_tick( @w_label_mm + @w_subscale_mm + @w_mainscale_mm, @h_mm, "%d" % ( 10 ** @size ) )
-      # label
+      render_tick( @start_mm + @w_mainscale_mm, @h_mm, "%d" % ( 10 ** @size ) )
+
       render_label( )
-      # add constants if any specified
       render_constants()
       render_subscale()
     end
 
-
     private
-    def render_constants() # disabled
+    def render_constants()
       @constants.each do | name, value |
-        x = @w_label_mm + @w_subscale_mm + Math.log10( value ) * @w_mainscale_mm / @size
+        x = @start_mm + Math.log10( value ) * @scale
         h = @h_mm * @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][1]
         render_tick( x, h, "%s" % name, Io::Creat::Slipstick::Entity::CONSTANT )
+      end
+    end
+
+    # fill the range with smallest ticks
+    private
+    def render_fodder( start_mm, end_mm, start_val, step )
+      delta = ( start_mm.abs - end_mm.abs ).abs
+      no_smallest = 0
+      @dim[Io::Creat::Slipstick::Key::FODDERS].each do | no |
+        if delta > no * @dim[Io::Creat::Slipstick::Key::CLEARING]
+          no_smallest = no
+          break
+        end
+      end
+
+      if no_smallest > 0
+        stepper = step / no_smallest
+        for k in 1..no_smallest - 1
+          mx = @start_mm + Math.log10( start_val + k * stepper ) * @scale
+          h = @h_mm * ( k % ( no_smallest / 5 )  == 0 ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][3] : @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][4] )
+          render_tick( mx, h, nil )
+        end
       end
     end
 
@@ -94,11 +94,11 @@ module Io::Creat::Slipstick
       end
 
       value = 1
-      last = @w_label_mm + @w_subscale_mm
+      last = @start_mm
       step = 0.02
       while true do
         value -= step
-        x = @w_label_mm + @w_subscale_mm + Math.log10( value ) * @w_mainscale_mm / @size
+        x = @start_mm + Math.log10( value ) * @scale
         if x <= @w_label_mm
           return
         end
@@ -106,23 +106,7 @@ module Io::Creat::Slipstick
         h = @h_mm * ( round ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][1] : @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][2] )
         render_tick( x, h, ( round ? ( "%.1f" % value )[1..-1] : nil ) )
 
-        # filler
-        delta = last - x
-        no_smallest = 0
-        @dim[Io::Creat::Slipstick::Key::FODDERS].each do | no |
-         if delta >= no * @dim[Io::Creat::Slipstick::Key::CLEARING]
-            no_smallest = no
-            break
-         end
-        end
-        if no_smallest > 0
-          stepper = step / no_smallest
-          for k in 1..no_smallest - 1
-            mx = @w_label_mm + @w_subscale_mm + Math.log10( value + k * stepper ) * @w_mainscale_mm / @size
-            h = @h_mm * ( k % ( no_smallest / 5 )  == 0 ? @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][3] : @dim[Io::Creat::Slipstick::Key::TICK_HEIGHT][4] )
-            render_tick( mx, h, nil )
-          end
-        end
+        render_fodder( last, x, value, 0.02 )
         last = x
       end
     end
