4d30a5b4a09eba946b78e9686b0f2c94be157919
Jan Stanik <js@creat.io>
2015-01-28 19:21:09 +0000

    Table of scales.

--- src/model_a.rb
+++ src/model_a.rb
@@ -252,22 +252,74 @@ module Io::Creat::Slipstick
 
       def render ( )
         my_mm = @y_mm - @h_mm / 2
-        w_mm = @fs_mm * 5
-        h_mm = 1.4 * @fs_mm
-        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm, @y_mm, @fs_mm * 0.2, Io::Creat::Slipstick::Style::DEFAULT[Io::Creat::Slipstick::Entity::LOTICK].merge( { Io::Creat::Slipstick::Key::FONT_SIZE => @fs_mm } ) )
+        w_mm = @fs_mm * 2.5
+        #h_mm = 1.4 * @fs_mm
+        h_mm = @h_mm / 12
+        fs_mm = h_mm / 1.4
+        style = Io::Creat::Slipstick::Style::DEFAULT[Io::Creat::Slipstick::Entity::LOTICK].merge( { Io::Creat::Slipstick::Key::FONT_SIZE => fs_mm } )
+        spacing = @fs_mm * 0.2
+        tables = []
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm, @y_mm, spacing, style )
           tr = table.tr( h_mm )
-            td = tr.td( 'L1', w_mm )
-            td = tr.td( 'L1', w_mm )
+            td = tr.td( 'L', w_mm )
+            td = tr.td( 'log', 2 * w_mm )
           tr = table.tr( h_mm )
-            td = tr.td( 'L1', w_mm )
-            td = tr.td( 'L1', w_mm )
+            td = tr.td( 'P', w_mm )
+            td = tr.td( '√1-x²', w_mm )
           tr = table.tr( h_mm )
-            td = tr.td( 'L1', w_mm )
-            td = tr.td( 'L1', w_mm )
+            td = tr.td( 'K', w_mm )
+            td = tr.td( 'x³', w_mm )
           tr = table.tr( h_mm )
-            td = tr.td( 'L1', w_mm )
-            td = tr.td( 'L1', w_mm )
-        table.render()
+            td = tr.td( 'A', w_mm )
+            td = tr.td( 'x²', w_mm )
+          tables << table
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm, @y_mm + 4.5 * h_mm, spacing, style )
+          tr = table.tr( h_mm )
+            td = tr.td( 'B', w_mm )
+            td = tr.td( 'x²', 2 * w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'CI', w_mm )
+            td = tr.td( '1/x', w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'C', w_mm )
+            td = tr.td( 'x', w_mm )
+          tables << table
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm, @y_mm + 8 * h_mm, spacing, style )
+          tr = table.tr( h_mm )
+            td = tr.td( 'D', w_mm )
+            td = tr.td( 'x', 2 * w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'S', w_mm )
+            td = tr.td( 'sin', w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'T', w_mm )
+            td = tr.td( 'tan', w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'ST', w_mm )
+            td = tr.td( 'sin-tan', w_mm )
+          tables << table
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm + 0.5 * h_mm + 3 * w_mm, @y_mm + 3 * h_mm, spacing, style )
+          tr = table.tr( h_mm )
+            td = tr.td( 'cm', 3 * w_mm )
+          tables << table
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm + 0.5 * h_mm + 3 * w_mm, @y_mm + 4.5 * h_mm, spacing, style )
+          tr = table.tr( h_mm )
+            td = tr.td( 'LL1', w_mm )
+            td = tr.td( 'e⁰·⁰¹ˣ', 2 * w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'LL2', w_mm )
+            td = tr.td( 'e⁰·¹ˣ', w_mm )
+          tr = table.tr( h_mm )
+            td = tr.td( 'LL3', w_mm )
+            td = tr.td( 'e¹ˣ', w_mm )
+          tables << table
+        table = Io::Creat::Slipstick::Graphics::Table.new( @img, @x_mm + 0.5 * h_mm + 3 * w_mm, @y_mm + 8 * h_mm, spacing, style )
+          tr = table.tr( h_mm )
+            td = tr.td( 'inches', 3 * w_mm )
+          tables << table
+        tables.each do | table |
+          table.render()
+        end
       end
 
       end # ScalesBackprint
