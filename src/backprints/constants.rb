3705d465d30106ad42555fa97918a986a85b9b7c
Jan Stanik <js@creat.io>
2015-01-29 21:57:24 +0000

    Play with physical constants.

--- src/backprints/constants.rb
+++ src/backprints/constants.rb
@@ -9,22 +9,26 @@ module Io::Creat::Slipstick
   module Backprints
 
     class ConstantsBackprint < Backprint
-      CONSTANTS =  [ [ 'imaginary',             'i²', "-1" ],
-                     [ 'Meissel–Mertens',       'M₁', "\u00a00.2614972128" ],
-                     [ 'Omega',                  'Ω', "\u00a00.5671432904" ],
-                     [ 'Euler-Mascheroni',       'γ', "\u00a00.5772156649" ],
-                     [ 'Apéry',                  'ζ', "\u00a01.2020569032" ],
-                     [ 'Pythagoras',       "\u221b2", "\u00a01.4142135624" ],
-                     [ 'Ramanujan-Soldner',      'μ', "\u00a01.4513692349" ],
-                     [ 'Golden ratio',           'φ', "\u00a01.6180339887" ],
-                     [ 'Theodorus',        "\u221b3", "\u00a01.7320508076" ],
-                     [ '',                 "\u221b5", "\u00a02.2360679775" ],
-                     [ 'Euler',                  'e', "\u00a02.7182818285" ],
-                     [ 'Archimedes',             'π', "\u00a03.1415926536" ],
-                     [ 'Reciprocal Fibonacci',   'ψ', "\u00a03.3598856662" ],
-                   ]
-      PHYS = [ [ 'Astronomic unit' ],
-               [ 'Speed of light' ] ]
+      MATH = [ [ 'imaginary',             'i²', "-1" ],
+               [ 'Meissel–Mertens',       'M₁', "\u00a00.2614972128" ],
+               [ 'Omega',                  'Ω', "\u00a00.5671432904" ],
+               [ 'Euler-Mascheroni',       'γ', "\u00a00.5772156649" ],
+               [ 'Apéry',                  'ζ', "\u00a01.2020569032" ],
+               [ 'Pythagoras',       "\u221b2", "\u00a01.4142135624" ],
+               [ 'Ramanujan-Soldner',      'μ', "\u00a01.4513692349" ],
+               [ 'Golden ratio',           'φ', "\u00a01.6180339887" ],
+               [ 'Theodorus',        "\u221b3", "\u00a01.7320508076" ],
+               [ '',                 "\u221b5", "\u00a02.2360679775" ],
+               [ 'Euler',                  'e', "\u00a02.7182818285" ],
+               [ 'Archimedes',             'π', "\u00a03.1415926536" ],
+               [ 'Reciprocal Fibonacci',   'ψ', "\u00a03.3598856662" ],
+             ]
+      PHYS = [ [ 'Astronomical unit', 'au', '149,597,870,700 m' ],
+               [ 'Speed of light',     'c', '299,792,458 m·s ⁻¹' ],
+               [ 'Gravitational',      'G', '6.67384(80)×10 ⁻¹¹ m³·kg ⁻¹·s ⁻²' ],
+               [ 'Planck',             'h', '6.626 069 57(29) × 10⁻³⁴ J·s' ],
+             ]
+
       def render ( )
         my_mm = @y_mm - @h_mm / 2
         w_mm = @fs_mm * 2.5
@@ -34,7 +38,7 @@ module Io::Creat::Slipstick
         spacing = @fs_mm * 0.2
         tables = []
         table = Table.new( @img, @x_mm, @y_mm, spacing, style )
-        CONSTANTS.each do | constant |
+        PHYS.each do | constant |
           tr = table.tr( h_mm )
             td = tr.td( constant[0], 4.5 * w_mm )
             td = tr.td( constant[1], 1 * w_mm, Td::MID )
