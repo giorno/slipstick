da08485422a15d1d0580ff4d44068a3a72feb816
Jan Stanik <js@creat.io>
2015-04-14 19:38:42 +0000

    SLIP-8 Localization template loading.

---
+++ src/i18n.rb
@@ -0,0 +1,33 @@
+
+# vim: et
+
+require 'singleton'
+require 'yaml'
+
+module Io
+  module Creat
+    module Slipstick
+
+      class I18N
+        include Singleton
+
+        def load ( file, lang = 'en' )
+          @strings = YAML.load_file( file )['en']
+        end
+
+        def string ( key )
+          raise "YML file not loaded" unless not @strings.nil?
+          if @strings.has_key?( key )
+            return @strings[key]
+          else
+            $stderr.puts "Translation for key not found: " + key
+            return key# failsafe
+          end
+        end
+
+      end # I18N
+
+    end # Slipstick
+  end # Creat
+end # Io
+
