780b148b976fe34bec4dd6d3b5b9db8f6407056c
Jan Stanik <js@creat.io>
2016-06-26 14:09:18 +0000

    SLIP-68 Separate the Stock component.

--- src/model_a.rb
+++ src/model_a.rb
@@ -187,6 +187,105 @@ module Io::Creat::Slipstick
 
     end # Slide
 
+    # Static component
+    class Stock < Component
+
+      def initialize ( parent, layer )
+        super( parent, layer )
+        if ( ( @layer & LAYER_FACE ) != 0 )
+        # bottom stock strip
+          strip = @parent.create_strip( @dm.x_mm, @dm.y_mm, @dm.hl_mm, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "D", 0.5 )
+              scale.set_params( 1 )
+              scale.add_constants( )
+              scale.set_overflow( 1.0 )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_SIN, "S", 0.33, true )
+              scale.set_style( @style_small )
+              scale.set_params( 90, 5, [ 1, 5, 10, 20 ] )
+              scale.set_flags( 0 )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_TAN, "T", 0.33, true )
+              scale.set_style( @style_small )
+              scale.set_params( 45, 5, [ 1, 5, 10, 20 ] )
+              scale.set_flags( 0 )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_SINTAN, "ST", 0.33, true )
+              scale.set_style( @style_small )
+              scale.set_params( 6, 0.5, [ 1.0 / 12.0, 0.5 ], 8 )
+              scale.set_flags( 0 )
+              scale.set_overflow( @dm.b_mm )
+
+          # top of the stock back
+          strip = @parent.create_strip( @dm.x_mm, @dm.y_mm + @dm.t_mm + @dm.hl_mm, 8, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_DECIMAL, "cm", 0.33 )
+              scale.set_params( 25 )
+              scale.set_overflow( @dm.b_mm )
+
+          # bottom of the stock back
+          strip = @parent.create_strip( @dm.x_mm, @dm.y_mm + @dm.t_mm + @dm.h_mm + @dm.hl_mm - 8, 8, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_INCH, "inches", 0.33, true )
+              scale.set_params( 10 )
+              scale.set_overflow( @dm.b_mm )
+
+          # top stock strip
+          strip = @parent.create_strip( @dm.x_mm, @dm.y_mm + 2 * @dm.t_mm + @dm.h_mm + @dm.hu_mm, @dm.hu_mm, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_DECIMAL, "L", 0.33 )
+              scale.set_params( 10 )
+              scale.set_overflow( @dm.b_mm )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_PYTHAG, "P", 0.33 )
+              scale.set_style( @style_small )
+              scale.set_params( )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "K", 0.33 )
+              scale.set_style( @style_small )
+              scale.set_params( 3 )
+              scale.set_flags( 0 )
+              scale.add_constants( )
+            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "A", 0.5, true )
+              scale.set_params( 2 )
+              scale.add_constants( )
+              scale.set_overflow( 1.0 )
+
+          # backprints
+          bp_border_mm = 12.0
+          bp_y_mm = @dm.y_mm + @dm.hl_mm + @dm.t_mm + bp_border_mm
+          bp_h_mm = @dm.h_mm - 2 * bp_border_mm
+          bp_x_mm = @dm.x_mm + 0.5 * bp_border_mm
+
+          # scales layout
+          lo = ScalesBackprint.new( @img, bp_x_mm, bp_y_mm, bp_h_mm )
+            @parent.bprints << lo
+            bp_x_mm += bp_border_mm / 2 + lo.getw()
+
+          # sin-cos help
+          gr = TrigonometricBackprint.new( @img, bp_x_mm, bp_y_mm, bp_h_mm )
+            @parent.bprints << gr
+            bp_x_mm += bp_border_mm / 2 + gr.getw()
+
+          # log help
+          gr = LogBackprint.new( @img, bp_x_mm, bp_y_mm, bp_h_mm )
+            @parent.bprints << gr
+            bp_x_mm += bp_border_mm / 2 + gr.getw()
+
+          # table of mathematical and physical constants
+          cbp = ConstantsBackprint.new( @img, bp_x_mm, bp_y_mm, bp_h_mm )
+            @parent.bprints << cbp
+            bp_x_mm += bp_border_mm / 2 + cbp.getw()
+
+          # QR code
+          # TODO refactor to inherit from Backprint
+          bottom_off_mm = 15.0
+          bottom_mm = @dm.y_mm + bottom_off_mm + @dm.hl_mm + @dm.t_mm
+          gr_size_mm = @dm.h_mm - ( 2 * bottom_off_mm )
+          qr = Qr.new( @img, 'http://wheel.creat.io/sr', 4, :h, bp_x_mm, bottom_mm, gr_size_mm, @style[Io::Creat::Slipstick::Entity::QR] )
+            @parent.bprints << qr
+
+          # page number
+          pn = PageNoBackprint.new( @img, @dm.x_mm + @dm.w_mm / 2, @dm.sh_mm - @dm.y_mm, 6, @style_pageno )
+            pn.sett( '%s + %s (210 g/m²)' % [ @i18n.string( 'part_stock' ), @i18n.string( 'part_cursor' )  ] )
+            @parent.bprints << pn
+        end
+      end # initialize
+
+    end # Stock
+
     # model A inspired by layout of LOGAREX 27403-II
     class A < Io::Creat::Slipstick::Layout::Sheet
       attr_accessor :dm, :img, :style, :bprints, :i18n
@@ -229,88 +328,9 @@ module Io::Creat::Slipstick
         @style_aux = Io::Creat::svg_dec_style_units( @style[Io::Creat::Slipstick::Entity::AUX], SVG_STYLE_TEXT )
 
         # scales of the stator
-        if ( ( @layers & LAYER_STOCK ) != 0 ) and ( ( @layers & LAYER_FACE ) != 0 )
-          # bottom stock strip
-          strip = create_strip( @dm.x_mm, @dm.y_mm, @dm.hl_mm, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "D", 0.5 )
-              scale.set_params( 1 )
-              scale.add_constants( )
-              scale.set_overflow( 1.0 )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_SIN, "S", 0.33, true )
-              scale.set_style( @style_small )
-              scale.set_params( 90, 5, [ 1, 5, 10, 20 ] )
-              scale.set_flags( 0 )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_TAN, "T", 0.33, true )
-              scale.set_style( @style_small )
-              scale.set_params( 45, 5, [ 1, 5, 10, 20 ] )
-              scale.set_flags( 0 )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_SINTAN, "ST", 0.33, true )
-              scale.set_style( @style_small )
-              scale.set_params( 6, 0.5, [ 1.0 / 12.0, 0.5 ], 8 )
-              scale.set_flags( 0 )
-              scale.set_overflow( @dm.b_mm )
-
-          # top of the stock back
-          strip = create_strip( @dm.x_mm, @dm.y_mm + @dm.t_mm + @dm.hl_mm, 8, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_DECIMAL, "cm", 0.33 )
-              scale.set_params( 25 )
-              scale.set_overflow( @dm.b_mm )
-
-          # bottom of the stock back
-          strip = create_strip( @dm.x_mm, @dm.y_mm + @dm.t_mm + @dm.h_mm + @dm.hl_mm - 8, 8, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_INCH, "inches", 0.33, true )
-              scale.set_params( 10 )
-              scale.set_overflow( @dm.b_mm )
-
-          # top stock strip
-          strip = create_strip( @dm.x_mm, @dm.y_mm + 2 * @dm.t_mm + @dm.h_mm + @dm.hu_mm, @dm.hu_mm, @dm.w_m_mm, @dm.w_l_mm, @dm.w_s_mm, @dm.w_a_mm )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LIN_DECIMAL, "L", 0.33 )
-              scale.set_params( 10 )
-              scale.set_overflow( @dm.b_mm )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::TGN_PYTHAG, "P", 0.33 )
-              scale.set_style( @style_small )
-              scale.set_params( )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "K", 0.33 )
-              scale.set_style( @style_small )
-              scale.set_params( 3 )
-              scale.set_flags( 0 )
-              scale.add_constants( )
-            scale = strip.create_scale( Io::Creat::Slipstick::ScaleType::LOG_DECIMAL, "A", 0.5, true )
-              scale.set_params( 2 )
-              scale.add_constants( )
-              scale.set_overflow( 1.0 )
-
-          # backprints
-          @bp_border_mm = 12.0
-          @bp_y_mm = @dm.y_mm + @dm.hl_mm + @dm.t_mm + @bp_border_mm
-          @bp_h_mm = @dm.h_mm - 2 * @bp_border_mm
-          @bp_x_mm = @dm.x_mm + 0.5 * @bp_border_mm
-
-          # scales layout
-          lo = ScalesBackprint.new( @img, @bp_x_mm, @bp_y_mm, @bp_h_mm )
-            @bprints << lo
-            @bp_x_mm += @bp_border_mm / 2 + lo.getw()
-
-          # sin-cos help
-          gr = TrigonometricBackprint.new( @img, @bp_x_mm, @bp_y_mm, @bp_h_mm )
-            @bprints << gr
-            @bp_x_mm += @bp_border_mm / 2 + gr.getw()
-
-          # log help
-          gr = LogBackprint.new( @img, @bp_x_mm, @bp_y_mm, @bp_h_mm )
-            @bprints << gr
-            @bp_x_mm += @bp_border_mm / 2 + gr.getw()
-
-          # table of mathematical and physical constants
-          cbp = ConstantsBackprint.new( @img, @bp_x_mm, @bp_y_mm, @bp_h_mm )
-            @bprints << cbp
-            @bp_x_mm += @bp_border_mm / 2 + cbp.getw()
-
-          # page number
-          pn = PageNoBackprint.new( @img, @dm.x_mm + @dm.w_mm / 2, @dm.sh_mm - @dm.y_mm, 6, @style_pageno )
-            pn.sett( '%s + %s (210 g/m²)' % [ @i18n.string( 'part_stock' ), @i18n.string( 'part_cursor' )  ] )
-            @bprints << pn
-       end
+        if ( ( @layers & LAYER_STOCK ) != 0 )
+          @component = Stock.new( self, @layers & 0x3 )
+        end
 
         # sides of the slide
         if ( ( @layers & LAYER_SLIDE ) != 0 )
@@ -406,7 +426,6 @@ module Io::Creat::Slipstick
       # render strips and edges for cutting/bending
       public
       def render()
-        @style_qr = @style[Io::Creat::Slipstick::Entity::QR]
         @style_cursor = @style[Io::Creat::Slipstick::Entity::LOTICK]
         @style = { :stroke_width => 0.1, :stroke => "black", :stroke_cap => "square", :fill => "none" }
         # [stock] lines are intentionally positioned upside down (in landscape)
@@ -436,12 +455,6 @@ module Io::Creat::Slipstick
             brand = PageNoBackprint.new( @img, @dm.x_mm + 174, @dm.y_mm + 105, HEIGHT_BRAND, @style_branding )
               brand.sett( "%s %s" % [ @i18n.string( 'slide_rule'), MODEL ], true )
               brand.render()
-            bottom_off_mm = 15.0
-            bottom_mm = @dm.y_mm + bottom_off_mm + @dm.hl_mm + @dm.t_mm
-            gr_size_mm = @dm.h_mm - ( 2 * bottom_off_mm )
-            # QR code
-            # TODO refactor to inherit from Backprint
-            qr = Qr.new( @img, 'http://wheel.creat.io/sr', 4, :h, @bp_x_mm, bottom_mm, gr_size_mm, @style_qr )
             @img.rtext( @dm.x_mm + @dm.w_mm - 5, @dm.y_mm + @dm.hl_mm + @dm.t_mm + @dm.h_mm / 2, -90, @version, Io::Creat::svg_dec_style_units( @style_branding, SVG_STYLE_TEXT ) )
           end
           if dir < 0 then rh_mm = 0 end
--- src/qr.rb
+++ src/qr.rb
@@ -2,31 +2,38 @@
 
 require 'rqrcode'
 require_relative 'constants'
+require_relative 'backprints/backprint'
 
 module Io::Creat::Slipstick
 
   # renders QR code onto Rasem SVG canvas
-  class Qr
+  class Qr < Io::Creat::Slipstick::Backprints::Backprint
     # mapping of QR size (version) to number of squares per dimension
     DENSITY = { 1 => 21.0, 2 => 25.0, 3 => 29.0, 4 => 33.0, 10 => 57.0, 25 => 117.0, 40 => 177.0 }
 
+    # style violates the Backprint contract
     public
     def initialize ( img, text, size, level, x_mm, y_mm, size_mm, style )
-      qr = RQRCode::QRCode.new( text, :size => size, :level => level )
-      @img = img
+      super( img, x_mm, y_mm, size_mm )
+      @qr = RQRCode::QRCode.new( text, :size => size, :level => level )
+      @style = style
       @scale = size_mm / DENSITY[ size ]
-      qr.modules.each_index do |x|
-        qr.modules.each_index do |y|
-          if qr.dark?( x, y )
-            @img.rectangle( "%g" % ( x_mm + y * @scale ),
-                            "%g" % ( y_mm + x * @scale ),
+    end # initialize
+
+    public
+    def render ( )
+      @qr.modules.each_index do |x|
+        @qr.modules.each_index do |y|
+          if @qr.dark?( x, y )
+            @img.rectangle( "%g" % ( @x_mm + y * @scale ),
+                            "%g" % ( @y_mm + x * @scale ),
                             "%g" % @scale,
                             "%g" % @scale,
-                            style )
+                            @style )
           end
         end
       end
-    end
+    end # render
 
   end # Qr
 
