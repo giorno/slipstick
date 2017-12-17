1e80534ce0c7edd2fd6705424bda53c894c16b0a
Jan Stanik <js@creat.io>
2017-12-17 15:08:53 +0000

    SLIP-51 Use thin Iosevka font-weight by default and make the branding on the stock face more prominent.

--- src/style/default.rb
+++ src/style/default.rb
@@ -11,18 +11,19 @@ module Io
     # instrument-like L&F.
     SVG_STYLE[:"font-family"] = 'Iosevka'
     SVG_STYLE[:"letter-spacing"] = 0
+    SVG_STYLE[:"font-weight"] = '300'
 
     module Slipstick
 
       DEFAULT = {
         Entity::TICK     => Io::Creat::SVG_STYLE,
-        Entity::HITICK   => Io::Creat::SVG_STYLE.merge( { :"font-weight" => 'bold' } ),
+        Entity::HITICK   => Io::Creat::SVG_STYLE.merge( { :"font-weight" => '500' } ),
         Entity::LOTICK   => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4 } ),
-        Entity::LABEL    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :"font-weight" => 'bold' } ),
+        Entity::LABEL    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :"font-weight" => '500' } ),
         Entity::SCALE    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4 } ),
         Entity::CONSTANT => Io::Creat::SVG_STYLE.merge( { :"font-style" => 'oblique', :"font-size" => 2.4, :fill => 'black' } ),
         Entity::UNITS    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :fill => 'black' } ),
-        Entity::BRANDING => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :fill => 'black' } ),
+        Entity::BRANDING => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :fill => 'black', :"font-weight" => '500' } ),
         Entity::PAGENO   => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :fill => 'black', :"stroke-width" => 0.22 } ),
         Entity::AUX      => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :"text-anchor" => 'middle' } ),
         Entity::QR       => { :fill => "black", :"stroke-width" => "0.01mm", :stroke => "black" },
