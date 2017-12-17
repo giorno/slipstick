78e13f06b0ad0fe025a6d9a6c4f34c880907c999
Jan Stanik <js@creat.io>
2017-12-17 22:17:55 +0000

    SLIP-51 Copy the style thickness from 'default' to 'colored' style.

--- src/style/colored.rb
+++ src/style/colored.rb
@@ -11,19 +11,20 @@ module Io
     # instrument-like L&F.
     SVG_STYLE[:"font-family"] = 'Iosevka'
     SVG_STYLE[:"letter-spacing"] = 0
+    SVG_STYLE[:"font-weight"] = '300'
 
     module Slipstick
 
       # derivation of the default style applying colors to more defined features
       COLORED = {
         Entity::TICK     => Io::Creat::SVG_STYLE,
-        Entity::HITICK   => Io::Creat::SVG_STYLE.merge( { :"font-weight" => 'bold', :fill => '#ff6f00' } ),
+        Entity::HITICK   => Io::Creat::SVG_STYLE.merge( { :"font-weight" => '500', :fill => '#ff6f00' } ),
         Entity::LOTICK   => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4 } ),
-        Entity::LABEL    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :fill => '#971c1c', :"font-weight" => 'bold' } ),
+        Entity::LABEL    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :fill => '#971c1c', :"font-weight" => '500' } ),
         Entity::SCALE    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4 } ),
         Entity::CONSTANT => Io::Creat::SVG_STYLE.merge( { :"font-style" => 'oblique', :"font-size" => 2.4, :fill => '#283593', :stroke => '#283593' } ),
         Entity::UNITS    => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :fill => '#878787', :stroke => '#878787' } ),
-        Entity::BRANDING => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :stroke => '#971c1c' } ),
+        Entity::BRANDING => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :"font-weight" => 500, :stroke => '#971c1c' } ),
         Entity::PAGENO   => Io::Creat::SVG_STYLE.merge( { :"text-anchor" => 'middle', :fill => 'black', :"stroke-width" => 0.22 } ),
         Entity::AUX      => Io::Creat::SVG_STYLE.merge( { :"font-size" => 2.4, :"text-anchor" => 'middle' } ),
         Entity::QR       => { :fill => "black", :"stroke-width" => "0.01mm", :stroke => "black" },
